0f711688210de7a9a75f31c0be31013f85e8ec38
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-03-08 18:07:03 +0200

    Add files via upload

---
+++ Exercice 2/Fruit.java
@@ -0,0 +1,7 @@
+package com.app;
+
+public class Fruit {
+    void eat(){
+        System.out.println("Eating the fruit...");
+    }
+}
---
+++ Exercice 2/Orange.java
@@ -0,0 +1,8 @@
+package com.app;
+
+public class Orange extends Fruit{
+    @Override
+    void eat() {
+        System.out.println("Pealing the orange and eating...");
+    }
+}
